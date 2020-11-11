9afa998fd85ec4c56da07b2eb8b1f4ded8ed3a62
Alina Aprel <alina.aprelh@mail.ru>
2020-11-11 14:34:27 +0300

    mongo has started

--- app.js
+++ app.js
@@ -2,9 +2,18 @@ const express = require('express');
 const path = require('path');
 const PORT = 3000;
 const app = express();
+const mongoose = require('mongoose');
 const routerCards = require('./routes/card.js');
 const routerUsers = require('./routes/user.js');
 const routerUsersId = require('./routes/userId.js');
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+    useFindAndModify: false
+})
+.then(()=> console.log('Mongo has started'))
+.catch(err => console.log(err))
 
 app.use('/', routerCards);
 app.use('/', routerUsers);
